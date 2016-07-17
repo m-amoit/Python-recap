4762641df6e1023862d5f8d4a5893829a4355bdc
Miriam Amoit <amoitjmiriam@gmail.com>
2016-07-17 13:26:32 +0300

    IF statement

---
+++ Recap/if.py
@@ -0,0 +1,14 @@
+number = 23
+guess = int(raw_input('Enter an integer: '))
+
+if guess == number :
+	print 'Congratulations, you guessed it.'
+	print '(But you do not win any prizes!)'
+
+elif guess < number:
+	print 'No, it is a little higher than that.' 
+
+else:
+	print 'No, it is a little lower than that.'
+
+print 'Done'
