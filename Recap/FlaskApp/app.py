eba10a07f611bc9ffa55e2a60fd9910f17f37f05
Miriam Amoit <amoitjmiriam@gmail.com>
2016-08-18 18:21:03 +0300

    <variable-name>

--- Recap/FlaskApp/app.py
+++ Recap/FlaskApp/app.py
@@ -1,9 +1,17 @@
 from flask import Flask 
 app = Flask(__name__)
 
-@app.route('/hello')
-def HelloWorld():
-	return 'Hello World'
+@app.route('/hello/<name>')
+def hello_name(name):
+	return 'Hello %s!' % name
 
-if __name__ == '__main__':
+@app.route('/blog/<int:postID>')
+def show_blog(postID):
+	return 'Blog number %d' % postID
+
+@app.route('/rev/<float:revNo>')
+def revision(revNo):
+	return 'Revision %f' % revNo
+
+if __name__=='__main__':
 	app.run()
