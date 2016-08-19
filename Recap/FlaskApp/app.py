9f791d7bd37f20cf367868b3dd0e89a7859b9d56
Miriam Amoit <amoitjmiriam@gmail.com>
2016-08-19 11:05:04 +0300

    Introducing templates

--- Recap/FlaskApp/app.py
+++ Recap/FlaskApp/app.py
@@ -1,18 +1,9 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/success/<name>')
-def success(name):
-	return '<html><body><h1>Welcome %s</h1></body></html>' % name
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-	if request.method=='POST':
-		user=request.form['nm']
-		return redirect(url_for('success', name=user))
-	else:
-		user=request.args.get('nm')
-		return redirect(url_for('success', name=user))
+@app.route('/hello/<user>')
+def index(user):
+	return render_template('hello.html', name=user)
 
 if __name__=='__main__':
 	app.run(debug=True)
