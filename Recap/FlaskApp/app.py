9e74e447a2115825afb32c9405d1bb61ddadc7cf
Miriam Amoit <amoitjmiriam@gmail.com>
2016-08-25 00:09:13 +0300

    static files

--- Recap/FlaskApp/app.py
+++ Recap/FlaskApp/app.py
@@ -1,21 +1,9 @@
-from flask import Flask, url_for
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	pass
+	return render_template('index.html')
 
-@app.route('/login')
-def login():
-	pass
-
-@app.route('/user/<username>')
-def profile(username):
-	pass
-
-with app.test_request_context():
-	print url_for('index')
-	print url_for('login')
-	print url_for('login', next='/')
-	print url_for('profile', username='Jane Doe')
-
+if __name__=='__main__':
+	app.run(debug=True)
