19e5de023ad704d966ba2a78766a371416307b7c
Miriam Amoit <amoitjmiriam@gmail.com>
2016-09-26 15:42:53 +0300

    Checking types

--- BuildingSkills/factorial.py
+++ BuildingSkills/factorial.py
@@ -5,7 +5,13 @@ def factorial(n):
 
 	3! == 3*2! = 3*2*1! = 3*2*1*0! = 3*2*1*1
 	'''
-	if n == 0:
+	if not isinstance(n, int):
+		print 'Factorial is only defined for integers.'
+		return None
+	elif n < 0:
+		print 'Factorial is not defined for negative integers.'
+		return None
+	elif n == 0:
 		return 1
 	else:
 		recurse = factorial(n-1)
