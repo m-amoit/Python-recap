e11494725fca7a62522d37614ac4c81be196793b
Miriam Amoit <amoitjmiriam@gmail.com>
2016-09-30 11:25:35 +0300

    Compute the absolute value of the difference

--- BuildingSkills/square_root.py
+++ BuildingSkills/square_root.py
@@ -1,13 +1,16 @@
 def square_root(a):
-	'''Takes a as a parameter, chooses a reasonable 
+	'''
+	Takes a as a parameter, chooses a reasonable 
 	value of x and uses it to estimate the square
-	root of a.'''
-	x = a/2.0
+	root of a using Newton's method.
+	Epsilon: a small value that determines how 
+	close is close enough.'''
+	x = a/3.0
+	epsilon = 0.00000001
 	while True:
 		print x
 		y = (x + a/x) / 2
-		if y == x:
+		if abs(y-x) < epsilon:
 			break
 		x = y
 
-square_root(9)
