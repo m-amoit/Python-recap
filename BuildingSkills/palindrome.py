675c669de254e1e389ee0ad90844fdf2565a5e1c
Miriam Amoit <amoitjmiriam@gmail.com>
2016-10-03 01:43:27 +0300

    Palindrome test using string slice

--- BuildingSkills/palindrome.py
+++ BuildingSkills/palindrome.py
@@ -1,33 +1,11 @@
 
-'''A word is a palindrome if the first and last
-letters are the same and the middle is a 
-palindrome'''
-def first(word):
-	'''Returns the first letter of a given string
-	'''
-	return word[0]
-
-def last(word):
-	'''Returns the last letter of a given string
-	'''
-	return word[-1]
-
-def middle(word):
-	'''Returns the middle part of a given string,
-	from the 2nd to the 2nd last letter'''
-	return word[1:-1]
-
 def is_palindrome(word):
 	'''Takes a string argument and returns True if
 	it is a palindrome and False otherwise
 	'''
-	if first(word) != last(word):
-		return False
-	if len(word) <= 2:
+	if word == word[::-1]:
 		return True
-	else:
-		word = middle(word)
-		return is_palindrome(word)
+	return False
 
 print is_palindrome('redivider')
 
