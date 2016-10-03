b728c6cb7e4d6eeb3bcf36427f5b536a9e9e7abd
Miriam Amoit <amoitjmiriam@gmail.com>
2016-10-03 13:59:01 +0300

    Search from a particular index

--- BuildingSkills/search.py
+++ BuildingSkills/search.py
@@ -1,9 +1,11 @@
-def find(word, letter):
+def find(word, letter, index):
 	'''Takes a character and finds the index where
-	that character appears'''
-	index = 0
+	that character appears.
+	Starts the seach from the given index'''
 	while index < len(word):
 		if word[index] == letter:
 			return index
 		index += 1
 	return -1
+
+print find('supersilious', 's', 3)
