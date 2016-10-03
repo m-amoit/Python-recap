34fb9675d81158eb6722973f378f1decf91ba1c2
Miriam Amoit <amoitjmiriam@gmail.com>
2016-10-03 22:34:39 +0300

    Using the string method count

--- BuildingSkills/count.py
+++ BuildingSkills/count.py
@@ -1,10 +1,6 @@
 def count(word, letter):
 	'''Counts the number of times a letter appears
 	in a string.'''
-	count = 0
-	for i in word:
-		if i == letter:
-			count += 1
-	return count 
+	return word.count(letter)
 
 print count('banana', 'a')
