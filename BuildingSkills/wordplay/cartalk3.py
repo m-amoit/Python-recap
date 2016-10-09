95306ba1961f050b0e30ed31434de12efd742625
Miriam Amoit <amoitjmiriam@gmail.com>
2016-10-09 18:20:56 +0300

    Check age differences with pallindromic mother daughter ages

--- BuildingSkills/wordplay/cartalk3.py
+++ BuildingSkills/wordplay/cartalk3.py
@@ -8,15 +8,28 @@ def reverse(i, j):
 	reverse of each other'''
 	return z_fill(i, 2) == z_fill(j, 2)[::-1]
 
-def is_reversed(daughter, diff):
-	'''Checks for pallindromic mother daughter ages.
-	'''
-	mother = daughter + diff
-	if mother == 120:
+def is_reversed(diff):
+	'''Checks for pallindromic mother daughter ages
+	and prints the number of times it happens in a 
+	mother's lifetime.'''
+	count = 0
+	daughter = 0
+	while True:
+		mother = daughter + diff
+		if reverse(daughter, mother):
+			count += 1
+		if mother == 120: break
+		daughter += 1
+	return count
+
+def check_diff(diff=0):
+	'''Check the age differences with pallindromic
+	mother daughter ages'''
+	x = is_reversed(diff)
+	if diff == 70:
 		return
-	if reverse(daughter, mother):
-		print daughter, mother
-	is_reversed(daughter+1, diff)
+	if x > 0:
+		print diff, x
+	check_diff(diff+1)
 
-
-is_reversed(0, 36)
+check_diff()
